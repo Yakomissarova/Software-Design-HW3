ab81332971b0aa414e956f1b11119120cd59fd7f
Yakomissarova <yakomissarova25@gmail.com>
2025-12-11 02:30:01 +0300

    Add readme

---
+++ AntiPlagiarism.CheckService.Infrastructure/Data/Migrations/20251210191021_AddSubmissionContentHash.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntiPlagiarism.CheckService.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubmissionContentHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentHash",
+                table: "Submissions",
+                type: "TEXT",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentHash",
+                table: "Submissions");
+        }
+    }
+}
